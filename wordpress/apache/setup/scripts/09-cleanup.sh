dee55d8a57a8b9757a51f4a1a88bf9776c1304c0
Supan Adit Pratama <email@supanadit.com>
2025-07-24 12:55:38 +0700

    feat: remove dev package

--- wordpress/apache/setup/scripts/09-cleanup.sh
+++ wordpress/apache/setup/scripts/09-cleanup.sh
@@ -10,6 +10,56 @@ rm -rf /tmp/downloads
 apt-get clean
 rm -rf /var/lib/apt/lists/* /var/cache/apt/archives
 
+echo "=== Removing development packages ==="
+apt-get remove -y \
+    build-essential \
+    autoconf \
+    automake \
+    autotools-dev \
+    binutils-common \
+    binutils-x86-64-linux-gnu \
+    binutils \
+    cpp-12 \
+    cpp \
+    dpkg-dev \
+    fakeroot \
+    g++-12 \
+    g++ \
+    gcc-12 \
+    gcc \
+    git-man \
+    git \
+    libc-dev-bin \
+    libc-devtools \
+    libc6-dev \
+    libcrypt-dev \
+    libcurl4-openssl-dev \
+    libexpat1-dev \
+    libgcc-12-dev \
+    libhashkit-dev \
+    libicu-dev \
+    libmemcached-dev \
+    libnsl-dev \
+    libonig-dev \
+    libpcre3-dev \
+    libsasl2-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libstdc++-12-dev \
+    libtirpc-dev \
+    libxml2-dev \
+    linux-libc-dev \
+    m4 \
+    make \
+    manpages-dev \
+    patch \
+    pkg-config \
+    pkgconf-bin \
+    pkgconf \
+    rpcsvc-proto \
+    zlib1g-dev \
+    icu-devtools
+
 # Remove unnecessary packages
 apt-get autoremove -y
 apt-get autoclean -y
