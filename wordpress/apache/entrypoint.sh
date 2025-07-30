b13225dcc581068ec351dae0e0feb63b77fc9c72
Supan Adit Pratama <email@supanadit.com>
2025-07-30 07:40:36 +0700

    fix: add protection rules for xmlrpc.php and wp-config.php in .htaccess

--- wordpress/apache/entrypoint.sh
+++ wordpress/apache/entrypoint.sh
@@ -141,6 +141,29 @@ RewriteRule . /index.php [L]
 </IfModule>
 # END WordPress
 EOF
+fi
+
+# If IS_PROTECT_XMLRPC is true, it will block xmlrpc.php requests
+if [ "$IS_PROTECT_XMLRPC" = "true" ]; then
+    if ! grep -q "deny from all" /var/www/html/.htaccess; then
+        echo "<Files xmlrpc.php>" >> /var/www/html/.htaccess
+        echo "    order deny,allow" >> /var/www/html/.htaccess
+        echo "    deny from all" >> /var/www/html/.htaccess
+        echo "</Files>" >> /var/www/html/.htaccess
+    fi
+fi
+
+if [ "$IS_PROTECT_WPCONFIG" = "true" ]; then
+    # If wp-config.php is not blocked, add rules to block it
+    if ! grep -q "<Files wp-config.php>" /var/www/html/.htaccess; then
+        echo "<Files wp-config.php>" >> /var/www/html/.htaccess
+        echo "    Require all denied" >> /var/www/html/.htaccess
+        echo "</Files>" >> /var/www/html/.htaccess
+    fi
+fi
+
+# If .htaccess exists, chown and set permissions
+if [ -f /var/www/html/.htaccess ]; then
     chown www-data:www-data /var/www/html/.htaccess
     # w3-total-cache need write access to .htaccess
     chmod 777 /var/www/html/.htaccess
@@ -167,5 +190,6 @@ chmod -R 777 /var/www/html/wp-content
 # This not secure but some plugins need write access to wp-config.php
 # For example, w3-total-cache
 chmod 777 /var/www/html/wp-config.php
+chown www-data:www-data /var/www/html/wp-config.php
 
 exec "$@"
