48c125ee81496ab15a6d591b44a417d1a3539153
Supan Adit Pratama <email@supanadit.com>
2025-07-31 10:50:35 +0700

    feat: set php memory limit on the fly

--- wordpress/apache/entrypoint.sh
+++ wordpress/apache/entrypoint.sh
@@ -183,6 +183,28 @@ if [ -n "$CUSTOM_TABLE_PREFIX" ]; then
     fi
 fi
 
+# Set PHP Memory Limit to PHP.ini
+if [ -n "$PHP_MEMORY_LIMIT" ]; then
+    if [ -f /usr/local/lib/php.ini ]; then
+        # Use AWK to replace existing memory_limit or add it if not found
+        awk -v limit="$PHP_MEMORY_LIMIT" '
+        BEGIN { found=0 }
+        /^[[:space:]]*memory_limit[[:space:]]*=/ {
+            print "memory_limit = " limit
+            found=1
+            next
+        }
+        {print}
+        END { 
+            if (!found) print "memory_limit = " limit 
+        }
+        ' /usr/local/lib/php.ini > /usr/local/lib/php.ini.tmp && mv /usr/local/lib/php.ini.tmp /usr/local/lib/php.ini
+    else
+        # If php.ini doesn't exist, create it with memory_limit
+        echo "memory_limit = $PHP_MEMORY_LIMIT" > /usr/local/lib/php.ini
+    fi
+fi
+
 # Set 777 permissions wp-content directory
 # I have no idea how to set proper permissions for wp-content directory
 # Some plugins doesn't wont running for example redis-object-cache
