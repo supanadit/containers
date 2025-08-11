bd6fc8c9685c75c5716b428e624288190b182fcd
Supan Adit Pratama <email@supanadit.com>
2025-08-11 13:32:52 +0700

    feat: handling prometheus.yml

--- prometheus/entrypoint.sh
+++ prometheus/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+if [ -f /config/prometheus.yml ]; then
+    if [ -f /etc/prometheus/prometheus.yml ]; then
+        rm /etc/prometheus/prometheus.yml
+    fi
+    cp /config/prometheus.yml /etc/prometheus/prometheus.yml
+fi
+
 exec "$@"
