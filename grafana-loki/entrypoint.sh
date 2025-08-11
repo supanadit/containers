2a4e1643605bcd8ab67496c7558140f2aac79697
Supan Adit Pratama <email@supanadit.com>
2025-08-11 17:44:21 +0700

    feat: update Grafana Loki entrypoint and configuration script for improved setup

--- grafana-loki/entrypoint.sh
+++ grafana-loki/entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+if [ ! -f /config/loki.yaml ]; then
+    cp /etc/loki/loki-sample.yaml /config/loki.yaml
+fi
+
 GRAFANA_LOKI_CONFIG=${GRAFANA_LOKI_CONFIG:-/etc/loki/loki.yaml}
 
+if [ ! -f ${GRAFANA_LOKI_CONFIG} ]; then
+    ln -sf /config/loki.yaml ${GRAFANA_LOKI_CONFIG}
+fi
+
 GRAFANA_LOKI_ARG_LIST=(
-    "/usr/share/grafana/loki/loki"
-    "serve"
-    "--config.file=${GRAFANA_LOKI_CONFIG}"
+    "/usr/share/grafana/loki"
+    "-config.file=${GRAFANA_LOKI_CONFIG}"
 )
 
 exec "${GRAFANA_LOKI_ARG_LIST[@]}"
--- grafana-loki/setup/scripts/03-configure-template.sh
+++ grafana-loki/setup/scripts/03-configure-template.sh
@@ -5,9 +5,9 @@ echo "=== Configure Grafana Loki Template ==="
 
 cd /tmp/downloads
 
-curl -LO "https://raw.githubusercontent.com/grafana/loki/v${GRAFANA_LOKI_VERSION}/cmd/loki/loki-local-config.yaml"
+curl -LO "https://raw.githubusercontent.com/grafana/loki/v${GRAFANA_LOKI_VERSION}/cmd/loki/loki-docker-config.yaml"
 
 mkdir -p /etc/loki
-mv loki-local-config.yaml /etc/loki/loki.yaml
+mv loki-docker-config.yaml /etc/loki/loki-sample.yaml
 
 echo "=== Configuring Grafana Loki successfully ==="
