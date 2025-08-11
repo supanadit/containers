2dc5e9946e7624239f26489f3c2334dae4d6f663
Supan Adit Pratama <email@supanadit.com>
2025-08-11 13:40:49 +0700

    feat: dynamically set prometheus port

--- prometheus/entrypoint.sh
+++ prometheus/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PROMETHEUS_PORT=${PROMETHEUS_PORT:-9090}
+
 if [ -f /config/prometheus.yml ]; then
     if [ -f /etc/prometheus/prometheus.yml ]; then
         rm /etc/prometheus/prometheus.yml
@@ -7,4 +9,7 @@ if [ -f /config/prometheus.yml ]; then
     cp /config/prometheus.yml /etc/prometheus/prometheus.yml
 fi
 
-exec "$@"
+exec prometheus \
+    --config.file=/etc/prometheus/prometheus.yml \
+    --storage.tsdb.path=/opt/prometheus/data \
+    --web.listen-address=":${PROMETHEUS_PORT}"
