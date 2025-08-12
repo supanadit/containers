fb36c0a883f908e516ad2d31f4d0ab87c82e9103
Supan Adit Pratama <email@supanadit.com>
2025-08-12 11:11:27 +0700

    feat: add support for enabling listening on all interfaces in Grafana Loki entrypoint

--- grafana-loki/entrypoint.sh
+++ grafana-loki/entrypoint.sh
@@ -10,6 +10,14 @@ if [ ! -f ${GRAFANA_LOKI_CONFIG} ]; then
     ln -sf /config/loki.yaml ${GRAFANA_LOKI_CONFIG}
 fi
 
+ENABLE_LISTEN_ALL_INTERFACE=${ENABLE_LISTEN_ALL_INTERFACE:-true}
+
+if [ "${ENABLE_LISTEN_ALL_INTERFACE}" = "true" ]; then
+    # It will check /config/loki.yaml on instance_addr: 127.0.0.1
+    # Then will change to instance_addr: 0.0.0.0
+    sed -i 's/instance_addr: 127.0.0.1/instance_addr: 0.0.0.0/g' /config/loki.yaml
+fi
+
 GRAFANA_LOKI_ARG_LIST=(
     "/usr/share/grafana/loki"
     "-config.file=${GRAFANA_LOKI_CONFIG}"
