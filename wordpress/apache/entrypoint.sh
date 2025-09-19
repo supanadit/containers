3c2c6a28b8c2c77fc6e7b12f48e7e3dfeb032a9a
Supan Adit Pratama <email@supanadit.com>
2025-09-19 15:03:53 +0700

    fix: add PHP upload_max_filesize and post_max_size configuration to php.ini

--- wordpress/apache/entrypoint.sh
+++ wordpress/apache/entrypoint.sh
@@ -164,6 +164,46 @@ if [ -n "$PHP_MEMORY_LIMIT" ]; then
     fi
 fi
 
+# Set PHP Upload Max Filesize to PHP.ini
+if [ -n "$PHP_UPLOAD_MAX_FILESIZE" ]; then
+    if [ -f /usr/local/lib/php.ini ]; then
+        awk -v limit="$PHP_UPLOAD_MAX_FILESIZE" '
+        BEGIN { found=0 }
+        /^[[:space:]]*upload_max_filesize[[:space:]]*=/ {
+            print "upload_max_filesize = " limit
+            found=1
+            next
+        }
+        {print}
+        END { 
+            if (!found) print "upload_max_filesize = " limit 
+        }
+        ' /usr/local/lib/php.ini > /usr/local/lib/php.ini.tmp && mv /usr/local/lib/php.ini.tmp /usr/local/lib/php.ini
+    else
+        echo "upload_max_filesize = $PHP_UPLOAD_MAX_FILESIZE" >> /usr/local/lib/php.ini
+    fi
+fi
+
+# Set PHP Post Max Size to PHP.ini
+if [ -n "$PHP_POST_MAX_SIZE" ]; then
+    if [ -f /usr/local/lib/php.ini ]; then
+        awk -v limit="$PHP_POST_MAX_SIZE" '
+        BEGIN { found=0 }
+        /^[[:space:]]*post_max_size[[:space:]]*=/ {
+            print "post_max_size = " limit
+            found=1
+            next
+        }
+        {print}
+        END { 
+            if (!found) print "post_max_size = " limit 
+        }
+        ' /usr/local/lib/php.ini > /usr/local/lib/php.ini.tmp && mv /usr/local/lib/php.ini.tmp /usr/local/lib/php.ini
+    else
+        echo "post_max_size = $PHP_POST_MAX_SIZE" >> /usr/local/lib/php.ini
+    fi
+fi
+
 # Choose Apache MPM: https://www.datadoghq.com/blog/monitoring-apache-web-server-performance
 APACHE_MPM=${APACHE_MPM:-event} # default to event if not set
 
