0605a64c92f23feab8718bed243e9edf9451434e
Supan Adit Pratama <email@supanadit.com>
2025-10-28 15:03:13 +0700

    feat(entrypoint): simplify Grafana entrypoint script and set default home path

--- docker/grafana/entrypoint.sh
+++ docker/grafana/entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
+set -e
 
-if [ ! -f /config/grafana.ini ]; then
-    cp /usr/share/grafana/conf/defaults.ini /config/grafana.ini
-fi
-
-if [ -f /config/grafana.ini ]; then
-    if [ -f /etc/grafana/grafana.ini ]; then
-        rm /etc/grafana/grafana.ini
-    fi
-    ln -sf /config/grafana.ini /etc/grafana/grafana.ini
-fi
-
-GRAFANA_HOME_PATH=${GRAFANA_HOME_PATH:-/usr/share/grafana}
+GRAFANA_HOME_PATH=${GRAFANA_HOME_PATH:-/var/lib/grafana}
 GRAFANA_CONFIG_PATH=${GRAFANA_CONFIG_PATH:-/etc/grafana/grafana.ini}
 
 GRAFANA_ARG_LIST=(
