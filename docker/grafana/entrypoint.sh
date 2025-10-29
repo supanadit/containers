ce40fa889a625f8035af7616072de13e7500e4b3
Supan Adit Pratama <email@supanadit.com>
2025-10-29 10:01:32 +0700

    feat(grafana): update Grafana version to 12.2.1 and enhance entrypoint script with improved config handling

--- docker/grafana/entrypoint.sh
+++ docker/grafana/entrypoint.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 set -e
 
-GRAFANA_HOME_PATH=${GRAFANA_HOME_PATH:-/var/lib/grafana}
-GRAFANA_CONFIG_PATH=${GRAFANA_CONFIG_PATH:-/etc/grafana/grafana.ini}
+export GF_PATHS_HOME=${GF_PATHS_HOME:-/usr/share/grafana}
+export GF_PATHS_DATA=${GF_PATHS_DATA:-/var/lib/grafana}
+export GF_PATHS_LOGS=${GF_PATHS_LOGS:-/var/log/grafana}
+export GF_PATHS_CONFIG=${GF_PATHS_CONFIG:-/etc/grafana/grafana.ini}
+
+
+# /usr/share/grafana/conf/defaults.ini is the default config file provided by Grafana
+# If /etc/grafana/grafana.ini does not exist, copy the default config
+if [ ! -f "${GF_PATHS_CONFIG}" ]; then
+    echo "=== Copying default Grafana config to ${GF_PATHS_CONFIG} ==="
+    cp /usr/share/grafana/conf/defaults.ini "${GF_PATHS_CONFIG}"
+fi
 
 GRAFANA_ARG_LIST=(
     "/usr/share/grafana/bin/grafana"
     "server"
-    "--homepath=${GRAFANA_HOME_PATH}"
-    "--config=${GRAFANA_CONFIG_PATH}"
+    "--homepath=${GF_PATHS_HOME}"
+    "--config=${GF_PATHS_CONFIG}"
+    "--packaging=docker"
+    "cfg:default.paths.data=${GF_PATHS_DATA}"
+    "cfg:default.paths.logs=${GF_PATHS_LOGS}"
+    "cfg:default.paths.plugins=${GF_PATHS_PLUGINS}"
 )
 
 exec "${GRAFANA_ARG_LIST[@]}"
