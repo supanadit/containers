b63e96abdf131239447f62657ca000d132cb2d04
Supan Adit Pratama <email@supanadit.com>
2025-10-29 15:56:36 +0700

    feat(entrypoint): improve container IP address handling in entrypoint script

--- docker/grafana-tempo/entrypoint.d/entrypoint.sh
+++ docker/grafana-tempo/entrypoint.d/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Get container IP address
+CONTAINER_IP=$(hostname -i)
+
 export GRAFANA_TEMPO_CONFIG=${GRAFANA_TEMPO_CONFIG:-/etc/tempo.yaml}
 
 export GRAFANA_TEMPO_DATA_DIR=${GRAFANA_TEMPO_DATA_DIR:-/var/lib/tempo}
@@ -9,7 +12,7 @@ export GRAFANA_TEMPO_STORAGE_BACKEND=${GRAFANA_TEMPO_STORAGE_BACKEND:-local}
 
 export GRAFANA_TEMPO_TARGET=${GRAFANA_TEMPO_TARGET:-scalable-single-binary}
 
-export GRAFANA_TEMPO_ADDRESS_FRONTEND_WORKER=${GRAFANA_TEMPO_ADDRESS_FRONTEND_WORKER:-(hostname -f):9095}
+export GRAFANA_TEMPO_ADDRESS_FRONTEND_WORKER=${GRAFANA_TEMPO_ADDRESS_FRONTEND_WORKER:-${CONTAINER_IP}:9095}
 
 # Configuring /etc/tempo.yaml
 /opt/container/entrypoint.d/scripts/init/configuration.sh
