0b53c084bf39b023ef25f60934a5756be678225a
Supan Adit Pratama <email@supanadit.com>
2025-11-21 21:52:19 +0700

    feat(loki): add TODO for generating loki.yaml configuration support

--- docker/grafana-loki/entrypoint.sh
+++ docker/grafana-loki/entrypoint.sh
@@ -11,6 +11,9 @@ if [ "${ENABLE_LISTEN_ALL_INTERFACE}" = "true" ]; then
     sed -i 's/instance_addr: 127.0.0.1/instance_addr: 0.0.0.0/g' /config/loki.yaml
 fi
 
+# TODO:
+# 1. Add support for generating /etc/loki/loki.yaml, similar to grafana-mimir, grafana-tempo 
+
 GRAFANA_LOKI_ARG_LIST=(
     "/usr/share/grafana/loki"
     "-config.file=${GRAFANA_LOKI_CONFIG}"
