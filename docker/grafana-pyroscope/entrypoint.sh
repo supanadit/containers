604e171d1777bc9badb3511716f0cf45dc34e5ad
Supan Adit Pratama <email@supanadit.com>
2025-12-18 11:49:40 +0700

    fix: remove unused API base URL argument from Grafana Pyroscope entrypoint script

--- docker/grafana-pyroscope/entrypoint.sh
+++ docker/grafana-pyroscope/entrypoint.sh
@@ -6,8 +6,7 @@ GRAFANA_PYROSCOPE_DATA=${GRAFANA_PYROSCOPE_DATA:-/var/lib/pyroscope/data}
 GRAFANA_PYROSCOPE_ARG_LIST=(
     "/usr/share/grafana/pyroscope"
     "-pyroscopedb.data-path=${GRAFANA_PYROSCOPE_DATA}"
-    "-api.base-url"
-    "/pyroscope"
+    "-target=all"
 )
 
 exec "${GRAFANA_PYROSCOPE_ARG_LIST[@]}"
