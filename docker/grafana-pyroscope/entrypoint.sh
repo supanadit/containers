929028529179d4bc535d79b2e4444d1cd702e2a9
Supan Adit Pratama <email@supanadit.com>
2025-12-18 11:45:43 +0700

    fix: add data path argument for Pyroscope in entrypoint script

--- docker/grafana-pyroscope/entrypoint.sh
+++ docker/grafana-pyroscope/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+GRAFANA_PYROSCOPE_DATA=${GRAFANA_PYROSCOPE_DATA:-/var/lib/pyroscope/data}
+
 GRAFANA_PYROSCOPE_ARG_LIST=(
     "/usr/share/grafana/pyroscope"
+    "-pyroscopedb.data-path=${GRAFANA_PYROSCOPE_DATA}"
     "-api.base-url"
     "/pyroscope"
 )
