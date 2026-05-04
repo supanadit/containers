6b678268712676e31ace3fef2f7959b86bed6324
Supan Adit Pratama <email@supanadit.com>
2026-05-04 07:38:44 +0700

    wip: fix mariadb loop restart

--- docker/mariadb/entrypoint.d/scripts/init/01-directories.sh
+++ docker/mariadb/entrypoint.d/scripts/init/01-directories.sh
@@ -8,37 +8,38 @@ source /opt/container/entrypoint.d/scripts/utils/validation.sh
 
 log_script_start "01-directories.sh"
 
-# Data directory
-export MARIADB_DATA_DIR="${MARIADB_DATA_DIR:-/var/lib/mysql}"
-export MARIADB_CONFIG_DIR="${MARIADB_CONFIG_DIR:-/etc/mysql/mariadb.conf.d}"
-export MARIADB_LOG_DIR="${MARIADB_LOG_DIR:-/var/log/mariadb}"
-export MARIADB_RUN_DIR="${MARIADB_RUN_DIR:-/run/mariadb}"
-export MARIADB_BACKUP_DIR="${MARIADB_BACKUP_DIR:-/var/lib/mariadb/backup}"
+setup_directories() {
+    export MARIADB_DATA_DIR="${MARIADB_DATA_DIR:-/var/lib/mysql}"
+    export MARIADB_CONFIG_DIR="${MARIADB_CONFIG_DIR:-/etc/mysql/mariadb.conf.d}"
+    export MARIADB_LOG_DIR="${MARIADB_LOG_DIR:-/var/log/mariadb}"
+    export MARIADB_RUN_DIR="${MARIADB_RUN_DIR:-/run/mariadb}"
+    export MARIADB_BACKUP_DIR="${MARIADB_BACKUP_DIR:-/var/lib/mariadb/backup}"
 
-# Create all required directories
-local directories=(
-    "$MARIADB_DATA_DIR"
-    "$MARIADB_CONFIG_DIR"
-    "$MARIADB_LOG_DIR"
-    "$MARIADB_RUN_DIR"
-    "$MARIADB_BACKUP_DIR"
-)
+    local directories=(
+        "$MARIADB_DATA_DIR"
+        "$MARIADB_CONFIG_DIR"
+        "$MARIADB_LOG_DIR"
+        "$MARIADB_RUN_DIR"
+        "$MARIADB_BACKUP_DIR"
+    )
 
-for dir in "${directories[@]}"; do
-    if [ ! -d "$dir" ]; then
-        log_info "Creating directory: $dir"
-        mkdir -p "$dir"
-        chown mysql:mysql "$dir"
-        chmod 0755 "$dir"
+    for dir in "${directories[@]}"; do
+        if [ ! -d "$dir" ]; then
+            log_info "Creating directory: $dir"
+            mkdir -p "$dir"
+            chown mysql:mysql "$dir"
+            chmod 0755 "$dir"
+        fi
+    done
+
+    if [ "${MARIADB_SSL_ENABLE:-false}" = "true" ]; then
+        local ssl_dir="/var/lib/mysql/ssl"
+        mkdir -p "$ssl_dir"
+        chown mysql:mysql "$ssl_dir"
+        chmod 0700 "$ssl_dir"
     fi
-done
 
-# Create SSL directory if SSL is enabled
-if [ "${MARIADB_SSL_ENABLE:-false}" = "true" ]; then
-    local ssl_dir="/var/lib/mysql/ssl"
-    mkdir -p "$ssl_dir"
-    chown mysql:mysql "$ssl_dir"
-    chmod 0700 "$ssl_dir"
-fi
+    log_script_end "01-directories.sh"
+}
 
-log_script_end "01-directories.sh"
+setup_directories
--- docker/mariadb/entrypoint.d/scripts/runtime/startup.sh
+++ docker/mariadb/entrypoint.d/scripts/runtime/startup.sh
@@ -9,19 +9,21 @@ log_script_start "startup.sh"
 
 export PATH="/usr/local/mariadb/bin:$PATH"
 export MARIADB_DATA_DIR="${MARIADB_DATA_DIR:-/var/lib/mysql}"
+export MARIADB_RUN_DIR="${MARIADB_RUN_DIR:-/run/mariadb}"
 
 log_info "Starting MariaDB server"
+log_info "Data dir: $MARIADB_DATA_DIR"
+log_info "Run dir: $MARIADB_RUN_DIR"
 
+mkdir -p "$MARIADB_RUN_DIR"
+mkdir -p /run/mysqld
 chown -R mysql:mysql "$MARIADB_DATA_DIR"
+chown -R mysql:mysql "$MARIADB_RUN_DIR"
+chown mysql:mysql /run/mysqld
 
-local galera_enabled="${MARIADB_GALERA_ENABLE:-false}"
+log_info "Checking directories..."
+ls -la /run/ 2>&1 | head -5
+ls -la "$MARIADB_DATA_DIR" 2>&1 | head -5
 
-if [ "$galera_enabled" = "true" ]; then
-    log_info "Starting MariaDB in Galera mode"
-    exec mariadbd --user=mysql --datadir="$MARIADB_DATA_DIR" --console
-else
-    log_info "Starting MariaDB in standalone mode"
-    exec mariadbd --user=mysql --datadir="$MARIADB_DATA_DIR" --console
-fi
-
-log_script_end "startup.sh"
+log_info "Launching mariadbd..."
+exec mariadbd --user=mysql --datadir="$MARIADB_DATA_DIR" --socket=/run/mysqld/mysqld.sock
